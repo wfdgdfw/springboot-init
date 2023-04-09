0899df5c7b7daf7b03a99d26da4dc069f13a70ae
nland <1977193977@qq.com>
2023-04-09 23:49:07 +0800

    Initial commit

---
+++ src/main/java/com/weng/project/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.weng.project.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.weng.project.model.entity.User;
+
+/**
+ * 用户数据库操作
+  */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
